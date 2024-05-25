1f781a36b654874da7e22840442a31169f8712c0
oika0722 <eir328509@gmail.com>
2024-05-25 23:26:03 +0300

    filter.h

--- main-progect/main-progect/filter.h
+++ main-progect/main-progect/filter.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "tel.h"
+#include <vector>
+std::vector<phone_call*> filter(std::vector<phone_call*>& array, bool (*check)(phone_call* element));
+bool check_call_by_tariff(phone_call* element);
+bool check_call_by_date(phone_call* element);
+
